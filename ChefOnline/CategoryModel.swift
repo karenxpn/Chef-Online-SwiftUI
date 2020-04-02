749a95300083f650e6ecc48bb41a624d5e0a2131
KALIMI <karenmirakyan@gmail.com>
2020-04-02 22:59:12 +0400

    first commit

---
+++ ChefOnline/CategoryModel.swift
@@ -0,0 +1,9 @@
+//
+//  CategoryModel.swift
+//  ChefOnline
+//
+//  Created by Karen Mirakyan on 4/2/20.
+//  Copyright © 2020 Karen Mirakyan. All rights reserved.
+//
+
+import Foundation
---
+++ ChefOnline/SelectedCategory.swift
@@ -0,0 +1,21 @@
+//
+//  SelectedCategory.swift
+//  ChefOnline
+//
+//  Created by Karen Mirakyan on 4/2/20.
+//  Copyright © 2020 Karen Mirakyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct SelectedCategory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectedCategory_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectedCategory()
+    }
+}
