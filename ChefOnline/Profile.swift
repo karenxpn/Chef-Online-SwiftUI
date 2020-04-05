b42289e5250300b3a2df733509e75e59558f686c
KALIMI <karenmirakyan@gmail.com>
2020-04-05 13:35:39 +0400

    Profile file added

---
+++ ChefOnline/Profile.swift
@@ -0,0 +1,21 @@
+//
+//  Profile.swift
+//  ChefOnline
+//
+//  Created by Karen Mirakyan on 4/5/20.
+//  Copyright © 2020 Karen Mirakyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Profile_Previews: PreviewProvider {
+    static var previews: some View {
+        Profile()
+    }
+}
